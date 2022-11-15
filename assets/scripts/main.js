304fd505cd81e45c94e69ec347aa875385c0127f
Raymond Romero <raymond50romero@gmail.com>
2022-11-14 17:07:06 -0800

    finished A1 and A2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -65,18 +65,23 @@ function initializeServiceWorker() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 async function getRecipes() {
-  // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
-  /**************************/
+  if (localStorage.getItem('recipes') != null) {
+    return JSON.parse(localStorage.getItem('recipies'))
+  } 
   // The rest of this method will be concerned with requesting the recipes
   // from the network
-  // A2. TODO - Create an empty array to hold the recipes that you will fetch
-  // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
-  //            has a great article on them. A promise takes one parameter - A
-  //            function (we call these callback functions). That function will
-  //            take two parameters - resolve, and reject. These are functions
-  //            you can call to either resolve the Promise or Reject it.
+  else {
+    // A2. TODO - Create an empty array to hold the recipes that you will fetch
+    var recipesArray = [];
+
+    // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
+    //            has a great article on them. A promise takes one parameter - A
+    //            function (we call these callback functions). That function will
+    //            take two parameters - resolve, and reject. These are functions
+    //            you can call to either resolve the Promise or Reject it.
+  }
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
